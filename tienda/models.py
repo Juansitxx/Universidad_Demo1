3b5be5120151892785392e4070e819d2f99f56b2
JuansitxxDEV <108484017+Juansitxx@users.noreply.github.com>
2026-03-06 00:34:45 +0000

    Second Commit

--- tienda/models.py
+++ tienda/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Producto(models.Model):
+class Produccion(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=8, decimal_places=2)
