931104e049d95cc655e92cc9949dd8b6630c446a
JuansitxxDEV <108484017+Juansitxx@users.noreply.github.com>
2026-03-06 09:47:25 +0000

    Changelog

--- academia/urls.py
+++ academia/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "tienda"
-
-urlpatterns = [
- path("", views.home, name="home"),  
- path("productos/", views.listado_productos, name="lista_productos"), 
- path("productos/<int:pk>/", views.detalle_producto, name="detalle_producto")
-]
-from django.urls import path
-from . import views
-
 app_name = "academia"
 
 urlpatterns = [
